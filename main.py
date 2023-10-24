9a9034fa67ee1717538974f6fcacb2d593ef9bbe
k1seul <117340073+k1seul@users.noreply.github.com>
2023-10-24 20:26:59 +0900

    random train added

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from simple_dqn_train import simple_dqn_train
 from model_based_train import model_based_train
 from single_dqn_train import single_dqn_train
-
+from rand_train import rand_train
+"""
 for i in range(8):
     simple_dqn_train(rand_seed=i, fixed_trial=i)
     single_dqn_train(rand_seed=i, fixed_trial=i)
@@ -14,9 +15,12 @@ for i in range(10, 20):
 
 simulation_num = [2, 5, 10, 15, 20, 30]
 simulation_max_episode = [30, 12, 6, 4, 3, 2]
-"""
+
 for i in range(6):
     for l in range(1):
         model_based_train(rand_seed=l, simulation_num=simulation_num[i], simulation_max_episode=simulation_max_episode[i])
 
 """
+
+for i in range(8):
+    rand_train(rand_seed=i, fixed_trial=i)
