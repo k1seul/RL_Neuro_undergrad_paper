cc8a934c24dc59db555884f75bdc83f275803ab3
k1seul <117340073+k1seul@users.noreply.github.com>
2023-11-17 16:22:19 +0900

    dqn weight save added

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ for rand_seed in range(20, 22):
 
 """
 
-for rand_seed in range(100, 102):
+for rand_seed in range(103, 104):
     curiosity_based_train(rand_seed=rand_seed, random_explore=True) 
     model_based_train(rand_seed = rand_seed, simulation_num = 5, simulation_max_episode = 12) 
 
